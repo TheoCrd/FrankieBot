913e32b72b4cd3de0722e0684ccbabed243af318
TheoCrd <theo.couard@hotmail.fr>
2023-11-14 22:00:10 +0800

    test

--- src/main.py
+++
@@ -1,5 +0,0 @@
-#Just a test function for CI
-def add(x, y):
-    return x + y
-
-print(add(3,4))
--- tests/test_frankiebot.py
+++
@@ -1,7 +0,0 @@
-from src.main import add
-
-#just a tester for the add function from main.py module
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
